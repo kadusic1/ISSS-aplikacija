2c2ab48a656c617b5d6a2c4ab7f78c6ecb8ec51b
kadusic <adi.kadusic.22@size.ba>
2023-04-30 02:09:37 +0200

    Dodani komentari i poboljsana ranija verzija

--- functions/helper.cpp
+++ functions/helper.cpp
@@ -5,32 +5,35 @@
 
 using namespace std;
 
-vector<string> load_person(ifstream& input) {
+// Funkcija koja ucitava red odredjene tabele i vraca vrijedonsti kolona tog reda 
+vector<string> load_person(ifstream& input, int columnt_count) {
+    // Pomocni string koji ce nam omoguciti inicijalizaciju vektora
     string helper;
+    // Vektor pomocu kojeg izdvajamo vrijednosti kolona zasebno
     vector<string> help;
-    if(input.is_open()) {
-        for(int i = 0; i < 8; i++) {
-            getline(input, helper, '+');
-            help.push_back(helper);
-        }
-    } else {
-        cout << "Error: greska u otvaranju datoteke\n";
+    // Izdvajamo svako vrijednost kolone i postavljamo je u vektor
+    for(int i = 0; i < columnt_count; i++) {
+        getline(input, helper, '+');
+        help.push_back(helper);
     }
+    // Vracamo vrijednost vektora
     return help;
 }
 
-int line_count(const string& file) {
+// Funkcija koja vraca broj linija u txt dokumentu na osnovu otvorenog dokumenta tj.
+// fstreama
+int line_count(ifstream& input) {
+    // Pocetni broj linija inicijaliziran na 0
     int count = 0;
+    // Pomocna varijabla za ucitavanje dummy linija
     string line;
-    ifstream input(file);
-    if(input.is_open()) {
-            while(getline(input, line)) {
-            count++;
-        } 
-    } else {
-        cout << "Error: greska u otvaranju datoteke\n";
-        return;
-    }
-    input.close();
+    // Petlja ce se vrtiti dok ima linija u dokumentu i povecavati brojac
+    while(getline(input, line)) {
+        count++;
+    } 
+    // Vracamo citanje streama na pocetak
+    input.clear();
+    input.seekg(0, ios::beg);
+    // Vraca se broj linija
     return count;
 }
--- functions/loginPage.cpp
+++ functions/loginPage.cpp
@@ -6,45 +6,75 @@
 
 using namespace std;
 
-const string LOGINFILE = "functions/showlogin.txt";
+// Deklaracija varijabli koje cemo koristiti
+const string LOGINFILE = "functions/showlogin.txt"; // Lokacija fajla sa tekstom za login
+// stranicu
+const string PERSONDATA = "database/Osobe.txt"; // lokacija tabele u bazi podataka za
+// osobe
+int EMAIL_INDEX = 3; // index kolone na kojem se nalazi email u tabeli Osobe.txt
+int PASSWORD_INDEX = 7; // index kolone na kojem se nalazi password u tabeli Osobe.txt
+int COLUMN_PERSONS = 8; // Broj kolona tabele Osobe.txt
 
-
+// Funkcija koja prikazuje osnovne podatke za login stranu
 void show_login() {
+    // Deklaracija input file streama asociranog sa datotekom showlogin.txt
     ifstream input(LOGINFILE);
+    // Pomocni string
     string line;
+    // Provjera da li je stream dobro otvoren
     if(input.is_open()) {
+        // Petlja se ponavlja sve do kraja datoteke i upisuje liniju datoteke u string line
         while(getline(input, line)) {
+            // Ispisujemo spremljenu liniju
             cout << line << "\n";
         }
-    } else {
+    } 
+    else {
+        // Else statement ukoliko se dokument nije ispravo otvorio
         cout << "Error: greska u otvaranju datoteke.\n";
     }
+    // Zatvaranje dokumenta
     input.close();
 }
 
+// Funkcija za pokusaj logovanja
 void try_login() {
-    ifstream input("database/Osobe.txt");
+    // Deklaracija input file streama asociranog sa datotekom Osobe.txt jer poredimo
+    // email i password
+    ifstream input(PERSONDATA);
+    // Pomocne varijable za unos
     string email;
     string password;
-
+    // Unos emaila i passworda
     cout << "Email: ";
     getline(cin, email);
     cout << "Password: ";
     getline(cin, password);
 
+    // Vektor koji će sadržavati sve unose jednog reda (vrijednosti kolona)
     vector<string> helper;
-    int lineCount = line_count("database/Osobe.txt");
+    // Inicijaliziramo varijablu lineCount u koju poharnjujemo broj redova u dokumentu
+    // osobe.txt
+    int lineCount = line_count(input);
+    // Ponavljamo onoliko puta koliko ima redova
     for(int i = 0; i < lineCount; i++) {
+        // Ova for petlja sluzi da ukoliko smo npr. na drugom redu da ispisemo jedan
+        // dummy red
         for(int j = 0; j < i-1; j++) {
             string dummy;
             getline(input,dummy);
         }
-        helper = load_person(input);
-        if(helper[3] == email && helper[7] == password) {
+        // Inicijaliziramo vektor sa sadržajima reda i odgovarajućim brojem kolona
+        helper = load_person(input, COLUMN_PERSONS);
+        // Poredimo da li je kolona 3 jednaka emailu i kolona 7 jednako passwordu
+        // Ukoliko jest trebamo ucitati odgovarajuci meni
+        if(helper[EMAIL_INDEX] == email && helper[PASSWORD_INDEX] == password) {
             cout << "Uspjesno\n";
+            // meni()
             break;
         }
     }
+    // Zatvaramo file stream
     input.close();
 }
 
